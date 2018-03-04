b255c3ca9408467c8cdfd344342801e08941be01
amalshehu <amalshehu@gmail.com>
2018-03-04 13:03:42 +0530

    Fix leap year

--- date-difference.js
+++ date-difference.js
@@ -2,9 +2,7 @@ function dateDifference(d1, m1, y1, d2, m2, y2, days) {
   const valid = [d1, m1, y1, d2, m2, y2].every(
     x => (x > 0 && Number.isInteger(x) ? true : false)
   );
-  if (!valid) {
-    return 'Invalid date';
-  }
+  if (!valid) return 'Invalid date';
   if (m1 == m2 && y1 == y2) return days + d2 - d1;
   days = days + getDaysInMonth(m1, y1) - d1 + 1;
   d1 = 1;
@@ -16,7 +14,8 @@ const getDaysInMonth = (month, year) =>
   month == 2
     ? isLeapYear(year) ? 29 : 28
     : [4, 6, 9, 11].includes(month) ? 30 : 31;
-const isLeapYear = year => (year % 4 === 0 ? true : false);
+const isLeapYear = year =>
+  year % 100 == 0 ? year % 400 === 0 : year % 4 === 0;
 
 console.log(dateDifference(16, 7, 1993, 3, 3, 2018, 0));
 console.log(dateDifference(20, 5, 2017, 3, 3, 2018, 0));
